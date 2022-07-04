8c3e77bae416bfffaf5deb0a7fe735c6a55f0bc9
Eazy <codeepoch@gmail.com>
2022-07-04 21:32:23 +0100

    Initial Commit

---
+++ store/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def product_list(request):
+    return Response('OK')
+
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
